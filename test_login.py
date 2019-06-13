d80abfc6ccf243edc1373540a3a9f2480a17dfb4
SonnySkyez <hnic85@gmail.com>
2019-06-13 18:51:00 +0200

    New test case - add tests numeration.

--- test_login.py
+++ test_login.py
@@ -12,7 +12,7 @@ This test suite contains 7 test cases.
 Tested URL:     https://polls-application.herokuapp.com/polls/'''
 
 
-class Test_no_user_no_pass(unittest.TestCase):
+class Test_1_no_user_no_pass(unittest.TestCase):
     '''Test case when there is no user and no password
     in login attempt by user.'''
     def setUp(self):
@@ -63,7 +63,7 @@ class Test_no_user_no_pass(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
-class Test_login_pass_no_user(unittest.TestCase):
+class Test_2_login_pass_no_user(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
@@ -115,7 +115,7 @@ class Test_login_pass_no_user(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
-class Test_login_user_no_pass(unittest.TestCase):
+class Test_3_login_user_no_pass(unittest.TestCase):
     '''Test case with right username and wrong password
     typed in by user.'''
     def setUp(self):
@@ -169,7 +169,7 @@ class Test_login_user_no_pass(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
-class Test_wrong_user_pass(unittest.TestCase):
+class Test_4_wrong_user_pass(unittest.TestCase):
     '''Test case with wrong username and right password
     typed in by user.'''
     def setUp(self):
@@ -222,7 +222,63 @@ class Test_wrong_user_pass(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
-class Test_wrong_user_wrong_pass(unittest.TestCase):
+class Test_5_user_wrong_pass(unittest.TestCase):
+    '''Test case tests situation, when username is right,
+    and password is wrong'''
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text("Admin").click()
+        driver.find_element_by_id("id_username").click()
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("Sonny")
+        driver.find_element_by_id("login-form").submit()
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("wrongpass")
+        driver.find_element_by_id("login-form").submit()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+
+class Test_6_wrong_user_wrong_pass(unittest.TestCase):
     '''Test case with wrong username and wrong password
     typed in by user.'''
     def setUp(self):
@@ -278,7 +334,7 @@ class Test_wrong_user_wrong_pass(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
-class Test_login_logout(unittest.TestCase):
+class Test_7_login_logout(unittest.TestCase):
     '''Test case with proper username and password;
     successfull login attempt + logout.'''
     def setUp(self):
