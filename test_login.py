577afb3539892e301b6111cb7e0cd0c99bc4b454
SonnySkyez <hnic85@gmail.com>
2019-06-13 18:18:50 +0200

    New test case - login attempt with right username and password.

--- test_login.py
+++ test_login.py
@@ -76,7 +76,7 @@ class Test_login_pass_no_user(unittest.TestCase):
         driver.find_element_by_link_text("Admin").click()
         driver.find_element_by_id("id_password").click()
         driver.find_element_by_id("id_password").clear()
-        driver.find_element_by_id("id_password").send_keys("xxxxxx")
+        driver.find_element_by_id("id_password").send_keys("XXXXXX")
         driver.find_element_by_id("login-form").submit()
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[3]").click()
@@ -185,7 +185,7 @@ class Test_wrong_user_pass(unittest.TestCase):
         driver.find_element_by_id("id_username").clear()
         driver.find_element_by_id("id_username").send_keys("sonnny")
         driver.find_element_by_id("id_password").clear()
-        driver.find_element_by_id("id_password").send_keys("xxxxxx")
+        driver.find_element_by_id("id_password").send_keys("XXXXXX")
         driver.find_element_by_id("login-form").submit()
 
 
@@ -241,7 +241,63 @@ class Test_wrong_user_wrong_pass(unittest.TestCase):
         driver.find_element_by_id("id_username").send_keys("Sonny")
         driver.find_element_by_id("login-form").submit()
         driver.find_element_by_id("id_password").clear()
-        driver.find_element_by_id("id_password").send_keys("xxxxxx")
+        driver.find_element_by_id("id_password").send_keys("xxx")
+        driver.find_element_by_id("login-form").submit()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+
+class Test_login_logout(unittest.TestCase):
+    '''Test case with proper username and password;
+    successfull login attempt + logout.'''
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text("Admin").click()
+        driver.find_element_by_id("id_username").click()
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("Sonny")
+        driver.find_element_by_id("login-form").submit()
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("XXXXXX")
         driver.find_element_by_id("login-form").submit()
 
 
