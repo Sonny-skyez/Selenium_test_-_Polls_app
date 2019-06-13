f42c654ee856af9a4ed11cf375f06a583a418360
SonnySkyez <hnic85@gmail.com>
2019-06-13 17:13:52 +0200

    New test cases test_vote_6 and test_vote_7.

--- test_votes.py
+++ test_votes.py
@@ -344,5 +344,60 @@ class Test_vote_6(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
+class Test_vote_7(unittest.TestCase):
+    '''Test case that test situation in witch
+    app user accessess 3 poll question from index pages
+    and do not cast any vote.'''
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text(u"Jaka kolorystyka strony byłaby najlepsza?").click()
+        driver.find_element_by_link_text(u"Powrót").click()
+        driver.find_element_by_link_text(u"Bootstrap / obecna forma szablonów CSS?").click()
+        driver.find_element_by_link_text(u"Powrót").click()
+        driver.find_element_by_link_text(u"Czy w aplikacji znajdują się jakieś widoczne błędy?").click()
+        driver.find_element_by_link_text(u"Powrót").click()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+
 if __name__ == "__main__":
     unittest.main()
