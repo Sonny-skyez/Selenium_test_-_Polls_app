0dff5c2995011e88c4e5a3c2c9605f5710953b28
SonnySkyez <hnic85@gmail.com>
2019-06-13 18:31:25 +0200

    New test case - edit question poll, its name, date, and answer.

--- test_add_edit_delete.py
+++ test_add_edit_delete.py
@@ -76,5 +76,81 @@ class Test_add_question(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class Test_edit_question(unittest.TestCase):
+    '''Testing of created poll question,
+    date and answer edition.'''
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text("Admin").click()
+        driver.find_element_by_id("id_username").click()
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("Sonny")
+        driver.find_element_by_id("login-form").submit()
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("XXXXXX")
+        driver.find_element_by_id("login-form").submit()
+        driver.find_element_by_link_text("Questions").click()
+        driver.find_element_by_link_text("Test").click()
+        driver.find_element_by_id("id_question_text").click()
+        driver.find_element_by_id("id_question_text").clear()
+        driver.find_element_by_id("id_question_text").send_keys("Test_2")
+        driver.find_element_by_link_text("Today").click()
+        driver.find_element_by_link_text("Now").click()
+        driver.find_element_by_id("id_choice_set-0-choice_text").click()
+        driver.find_element_by_id("id_choice_set-0-choice_text").clear()
+        driver.find_element_by_id("id_choice_set-0-choice_text").send_keys("Test_2")
+        driver.find_element_by_id("question_form").submit()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+
 if __name__ == "__main__":
     unittest.main()
