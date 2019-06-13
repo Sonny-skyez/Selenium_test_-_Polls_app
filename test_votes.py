aa4238ae25d56107fb1caa81d9fffef2e38320e9
SonnySkyez <hnic85@gmail.com>
2019-06-13 16:40:40 +0200

    New test case test_vote_5 - 2 proper votes at the same answer.

--- test_votes.py
+++ test_votes.py
@@ -232,7 +232,7 @@ class Test_vote_5(unittest.TestCase):
     '''Test failed vote and next - proper casted
     vote by application user. Then Back to index page.'''
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.verificationErrors = []
         self.accept_next_alert = True
@@ -249,6 +249,66 @@ class Test_vote_5(unittest.TestCase):
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Obecne CSS'])[1]/following::input[1]").click()
         driver.find_element_by_link_text("Ankiety").click()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+
+class Test_vote_6(unittest.TestCase):
+    '''Test case testig 2-times proper casted votes
+    at the same answer by application user.'''
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text(u"Czy ta aplikacja powinna posiadać testy w Selenium?").click()
+        driver.find_element_by_xpath(
+            u"(.//*[normalize-space(text()) and normalize-space(.)='Czy ta aplikacja powinna posiadać testy w Selenium?'])[1]/following::label[1]").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Nie wiem'])[1]/following::input[1]").click()
+        driver.find_element_by_link_text(u"Powrót").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Tak'])[1]/following::label[1]").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Nie wiem'])[1]/following::input[1]").click()
+        driver.find_element_by_link_text(u"Powrót").click()
+        driver.find_element_by_link_text(u"Powrót").click()
 
 
     def is_element_present(self, how, what):
