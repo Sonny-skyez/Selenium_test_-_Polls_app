9cc94034b1b4780c2c00c09b0b561c0928e998cf
SonnySkyez <hnic85@gmail.com>
2019-06-13 17:43:37 +0200

    New test case - login attempt with wrong username and wrong password.

--- test_login.py
+++ test_login.py
@@ -170,6 +170,8 @@ class Test_login_user_no_pass(unittest.TestCase):
 
 
 class Test_wrong_user_pass(unittest.TestCase):
+    '''Test case with wrong username and right password
+    typed in by user.'''
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
@@ -182,6 +184,62 @@ class Test_wrong_user_pass(unittest.TestCase):
         driver.find_element_by_link_text("Admin").click()
         driver.find_element_by_id("id_username").clear()
         driver.find_element_by_id("id_username").send_keys("sonnny")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("xxxxxx")
+        driver.find_element_by_id("login-form").submit()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+
+class Test_wrong_user_wrong_pass(unittest.TestCase):
+    '''Test case with wrong username and wrong password
+    typed in by user.'''
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text("Admin").click()
+        driver.find_element_by_id("id_username").click()
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("Sonny")
+        driver.find_element_by_id("login-form").submit()
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys("xxxxxx")
         driver.find_element_by_id("login-form").submit()
