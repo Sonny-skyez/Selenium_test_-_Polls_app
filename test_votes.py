6b7e4e2f84f61336d4f90e7e6cc490a1af3cb20a
SonnySkyez <hnic85@gmail.com>
2019-06-13 16:05:47 +0200

    Create new test case test_vote_2 - no votes casted.

--- test_votes.py
+++ test_votes.py
@@ -4,7 +4,7 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
 
-'''Test Votes - test suite, that tests all possible methods
+'''Test Votes - first test suite, that tests all possible methods
 of casting votes by user in Polls web application.
 This suite contains 7 test cases.
 
@@ -17,12 +17,11 @@ class Test_vote_1(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
 
 
-    def test_app_dynamics_job(self):
+    def test_app(self):
         driver = self.driver
         driver.get("https://polls-application.herokuapp.com/polls/")
         driver.find_element_by_link_text(u"Czy w aplikacji znajdują się jakieś widoczne błędy?").click()
@@ -31,6 +30,58 @@ class Test_vote_1(unittest.TestCase):
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Nadal szukam!'])[1]/following::input[1]").click()
         driver.find_element_by_link_text("Ankiety").click()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+
+class Test_vote_2(unittest.TestCase):
+    '''This test case tests how application
+    will work, when user won't tast any vote.'''
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text(u"Czy ta aplikacja powinna posiadać testy w Selenium?").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Nie wiem'])[1]/following::input[1]").click()
+        driver.find_element_by_link_text(u"Powrót").click()
 
 
     def is_element_present(self, how, what):
