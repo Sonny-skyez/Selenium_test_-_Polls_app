108563e76c8ff2781f4962278496f3030634d99a
SonnySkyez <hnic85@gmail.com>
2019-06-13 16:18:31 +0200

    Create new test case test_vote_5 - failed vote, next proper vote by user.

--- test_votes.py
+++ test_votes.py
@@ -228,8 +228,61 @@ class Test_vote_4(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
-if __name__ == "__main__":
-    unittest.main()
+class Test_vote_5(unittest.TestCase):
+    '''Test failed vote and next - proper casted
+    vote by application user. Then Back to index page.'''
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_app(self):
+        driver = self.driver
+        driver.get("https://polls-application.herokuapp.com/polls/")
+        driver.find_element_by_link_text(u"Bootstrap / obecna forma szablonów CSS?").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Obecne CSS'])[1]/following::input[1]").click()
+        driver.find_element_by_xpath(
+            u"(.//*[normalize-space(text()) and normalize-space(.)='Nie wybrałeś żadnej opcji.'])[1]/following::label[1]").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Obecne CSS'])[1]/following::input[1]").click()
+        driver.find_element_by_link_text("Ankiety").click()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
